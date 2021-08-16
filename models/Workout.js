fb19e62f1202521d88c4c645102194a332295f66
Josh Gumperz <joshg3023@gmail.com>
2021-08-15 17:30:19 -0700

    setup the basic file structure and added models

---
+++ models/Workout.js
@@ -0,0 +1,26 @@
+const mongoose = require("mongoose");
+
+const Schema = mongoose.Schema;
+
+const ExerciseSchema = new Schema({
+    type: String,
+    name: String,
+    duration: Number,
+    weight: Number,
+    reps: Number,
+    sets: Number,
+    distance: Number
+});
+
+const WorkoutSchema = new Schema({
+    day: {
+        type: Date,
+        default: Date.now,
+        required: true
+    },
+    exercises: [ExerciseSchema]
+});
+
+const Workout = mongoose.model("Workout", WorkoutSchema);
+
+module.exports = Workout;
---
+++ server.js
@@ -0,0 +1,20 @@
+const express = require("express");
+const logger = require("morgan");
+const mongoose = require("mongoose");
+const routes = require('./controllers');
+
+const app = express();
+const PORT = process.env.PORT || 3001;
+
+app.use(logger("dev"));
+
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+
+app.use(express.static("public"));
+
+mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/workout", { useNewUrlParser: true });
+app.use(routes);
+app.listen(PORT, () => {
+  console.log(`Now listening to: http://localhost:${PORT}`);
+});
