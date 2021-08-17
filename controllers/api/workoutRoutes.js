eb62a8a3a2d705bc43df8a39e1bd1ce83b8f6ffe
Josh Gumperz <joshg3023@gmail.com>
2021-08-16 21:08:19 -0700

    added route for stats range

--- controllers/api/workoutRoutes.js
+++ controllers/api/workoutRoutes.js
@@ -1,6 +1,6 @@
 const router = require('express').Router();
 const mongojs = require("mongojs");
-const { Workout } = require("../../models/Workout");
+const { Workout } = require("../../models");
 
 // Get all workouts in ascending order
 router.get('/', async (req, res) => {
@@ -45,5 +45,23 @@ router.post('/', async (req, res) => {
 }
 );
 
+// Get workouts for last 7 days
+router.get('/range', async (req, res) => {
+    // Use $addFields to get the total duration for the workout https://docs.mongodb.com/manual/reference/operator/aggregation/addFields/#mongodb-pipeline-pipe.-addFields
+    Workout.aggregate([
+        { $addFields: { totalDuration: { $sum: "$exercises.duration" } } },
+        { $sort: { day: -1 } }, // Get the newest results
+        { $limit: 7 },  // Limit the results to the last 7 workouts
+        { $sort: { day: 1 } } // Put the oldest one at the end
+    ],
+        (err, data) => {
+            if (err) {
+                console.log(err);
+            } else {
+                console.log(data);
+                res.json(data);
+            }
+        });
+});
 
 module.exports = router;
